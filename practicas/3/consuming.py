649f8eaa8f5988ebea3d6b9a1892a504bf2a5c0c
Guille31794 <guille.g94@hotmail.es>
2019-04-20 21:11:55 +0200

    Deleted public credentials

--- practicas/3/consuming.py
+++ practicas/3/consuming.py
@@ -2,10 +2,14 @@ import pika
 import json
 import twitter
 
-api = twitter.Api(consumer_key='qrxCRvTesdtF7h9TCna1I1GHn',
-                  consumer_secret='nbQYCYZLCWSw5lGmnaR1lH7A3wgy9WDMEp0K4bbfOeW22avMa6',
-                  access_token_key='1105477055477678080-ZBsM2hBBczpYfpLdLZqTByLXzRdQQO',
-                  access_token_secret='yeMP0jjWeeQUVzi1YwXq151R0Gvx5G8YyWFK2azottfUt')
+#Global data needed
+with open('twitter_credentials.json', 'r') as read_file:
+    credentials = json.load(read_file)
+
+api = twitter.Api(consumer_key=credentials["CONSUMER_KEY"],
+                  consumer_secret=credentials["CONSUMER_SECRET"],
+                  access_token_key=credentials["ACCESS_TOKEN_KEY"],
+                  access_token_secret=credentials["ACCESS_TOKEN_SECRET"])
 
 commit_data = {}
 json_data = {}
@@ -22,7 +26,7 @@ def on_consuming(channel, method, properties, body):
     channel.cancel()
     on_twitter_publishing(json_data)
 
-def on_twitter_publishing(data):
+#def on_twitter_publishing(data):
 
 
 parameters = pika.ConnectionParameters('localhost')
