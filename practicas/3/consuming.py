8534ffdff36278d1d381a128d75eac7cd47f342f
Guille31794 <guille.g94@hotmail.es>
2019-04-26 08:58:19 +0200

    Changes for testing

--- practicas/3/consuming.py
+++ practicas/3/consuming.py
@@ -16,7 +16,7 @@ apiTwitter = twitter.Api(consumer_key=credentials['twitter']["CONSUMER_KEY"],
                   access_token_secret=credentials['twitter']["ACCESS_TOKEN_SECRET"])
 
 #Login in Dropbox
-apiDropbox = #Continue here..
+#apiDropbox = #Continue here..
 
 commit_data = {}
 json_data = {}
@@ -24,25 +24,36 @@ json_data = {}
 #Here I receive the message from rabbit    
 def on_consuming(channel, method, properties, body):
     #Storage data and dump it from json
+    type(body);
+    print("\n\n");
     commit_data = json.loads(body)
     #Queues have to confirm message delivery
     channel.basic_ack(delivery_tag=method.delivery_tag)
     #Consumers needs to be closed or they'll be iterating forever
     channel.cancel()
-    on_twitter_publishing(commit_data)
-    on_dropbox_storing(commit_data) #Function for Teodoro
+    type(commit_data);
+    print("\n\n");
+    print(commit_data);
+    #on_twitter_publishing(commit_data)
+    #on_dropbox_storing(commit_data) #Function for Teodoro
+    #on_drive_storing(commit_data)
 
 def on_twitter_publishing(data):
-    url = urllib.parse.quote_plus(data['url'])
-    status = apiTwitter.PostUpdate(status='Hello world!\n'+data['url'])
+    #url = urllib.parse.quote_plus(data['url'])
+    status = apiTwitter.PostUpdate(status='New Commit from: ' + 'commiter' +  ' on: ' + 'repoName' + 
+    ' follow link to discover the changes ' + 'repoURL')
 
-def on_dropbox_storing(data): #Complete this function, Teo
+def on_dropbox_storing(data): #Complete these functions, Teo
+    pass;
+
+def on_drive_storing(data):
+    pass;
 
 #Rabbitmq connection
 parameters = pika.ConnectionParameters('localhost')
 connection = pika.BlockingConnection(parameters)
 channel = connection.channel()
-channel.basic_consume(queue='Testing', on_message_callback=on_consuming)
+channel.basic_consume(queue='github', on_message_callback=on_consuming)
 
 #Spooling
 try:
